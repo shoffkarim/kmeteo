34903651eca0315020c82d717d65270419d5026e
shoffkarim <k4r1mch1k@gmail.com>
2020-12-01 14:16:47 +0500

    fetch xml

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Welcome from "./mainPage/welcome";
+
 export default class App extends Component {
+
+
+
   render() {
     return (
       <div className="container">
-        <Welcome></Welcome>
-        <input placeholder="choose your city"></input>
+        <Welcome city="chelyabinsk"></Welcome>
+
       </div>
     );
   }
--- src/mainPage/welcome.js
+++ src/mainPage/welcome.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
+import axios  from "axios";
+// import { weather } from "../data.json";
 
+
+// http://api.worldweatheronline.com/premium/v1/weather.ashx?key=3722e3bc2da944118e854829202411&q=Chelyabinsk,russia&num_of_days=2&tp=3&format=xml
 export default class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: "lol",
+    }
+
+
+  }
+
+  componentDidMount() {
+    axios
+    .get("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=3722e3bc2da944118e854829202411&q=Chelyabinsk,russia&num_of_days=2&tp=3&format=xml", {
+      "Content-Type": "application/xml; charset=utf-8"
+     })
+    .then(response => {
+      this.setState({ data: response.data })
+    })
+
+  }
 
   render() {
     return (
       <div className="welcome-container">
-        <h1 className="welcome__heading">Welcome to kmeteo</h1>
+        <h1 className="welcome__heading">Welcome to kmeteo</h1>пше
+        <p>{ this.state.data }</p>
         <p className="welcome__text">Please, choose your city</p>
+        <input placeholder="choose your city"></input>
       </div>
     );
   }
