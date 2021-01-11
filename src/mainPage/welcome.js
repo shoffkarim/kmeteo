0901000625d08d4b819ae6e80b79fdf639226774
shoffkarim <k4r1mch1k@gmail.com>
2021-01-11 15:04:00 +0500

    simple realization

--- src/mainPage/welcome.js
+++ src/mainPage/welcome.js
@@ -9,9 +9,11 @@ export default class Welcome extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: "lol",
-      xml: "kek",
-      value: ""
+      data: "",
+      xml: "",
+      value: "",
+      city: "",
+      temp_C: ""
     }
 
     this.getData = this.getData.bind(this);
@@ -30,9 +32,12 @@ export default class Welcome extends Component {
     .then(response => {
       this.setState({ data: response.data })
       var parser = new xml2js.Parser();
-      parser.parseString(this.state.data, (err, result) => this.setState({ xml: JSON.stringify(result) }));
+      parser.parseString(this.state.data, (err, result) => this.setState({ xml: result }));
+      this.setState({city: this.state.xml.data.request[0].query})
+      this.setState({temp_C: this.state.xml.data.current_condition[0].temp_C})
+      console.log(this.state.xml.data.current_condition[0].temp_C)
     })
-    console.log(this.state.xml)
+
     event.preventDefault();
 
   }
@@ -41,8 +46,9 @@ export default class Welcome extends Component {
   render() {
     return (
       <div className="welcome-container">
-        <h1 className="welcome__heading">Welcome to kmeteo</h1>пше
-        <p>{ this.state.xml }</p>
+        <h1 className="welcome__heading">Welcome to kmeteo</h1>
+        <p>{ this.state.city }</p>
+        <p>{ this.state.temp_C }</p>
         <p className="welcome__text">Please, choose your city</p>
         <form onSubmit={this.getData}>
           <input type="text" className="choose-city" value={ this.state.value } onChange={ this.handleChange }></input>
